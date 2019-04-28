aca9f379b164aee8c5e649198460da5a70f74035
Alex-Martin <amkl17mz@gmail.com>
2019-04-28 19:58:08 +0300

    Fixed typos

--- LCD16d2.ino
+++ LCD16d2.ino
@@ -1,16 +1,11 @@
-#include <LCD8x2.h>
+#include <LCD16d2.h>
 
 void setup() {
+  for(a=0;a<=7;a++) {
+	pinMode(a,OUTPUT);
+  }
   pinMode(12,OUTPUT);
   pinMode(11,OUTPUT);
-  pinMode(0,OUTPUT);
-  pinMode(1,OUTPUT);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
   Init();
   Write_String("Bigger than 8ddd");
 }
